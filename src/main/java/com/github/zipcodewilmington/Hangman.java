37e66e082648835f23e44475f0afaf14b5922575
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-07-03 19:34:30 -0400

    oh i wrote a buncha code LOL

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,6 +1,8 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.ArrayList;
+
 /**
  * @author xt0fer
  * @version 1.0.0
@@ -8,4 +10,40 @@ package com.github.zipcodewilmington;
  */
 public class Hangman {
     // hi i'm adding my name Christine Gao
+    // final vars AKA constant
+    private final int MAX_GUESSES = 4;
+    // Keep track of the guesses
+    private int guessNum;
+    private ArrayList<Character> listOfGuesses;
+    private String[] wordBank = {"freedom", "contract", "eternity", "wisdom", "justice", "war", "angst"};
+
+    /*
+     * Constructor!
+     */
+    public Hangman(){
+        guessNum = 0;
+        listOfGuesses = new ArrayList<Character>();
+    }
+
+    public boolean isNotMaxGuess() {
+        return guessNum != MAX_GUESSES;
+    }
+
+    /*
+     * Print dashes for letters not found and the letter itself for letters found.
+     */
+    public boolean printGuess() {
+        return false;
+    }
+
+    public void makeGuess(Character curGuess) {
+        // you have already guessed this letter before
+        if(listOfGuesses.contains(curGuess)){
+            System.out.println("This was a previous guess!");
+            return;
+        }
+
+        listOfGuesses.add(curGuess);
+        guessNum += 1;
+    }
 }
---
+++ src/main/java/com/github/zipcodewilmington/Main.java
@@ -0,0 +1,79 @@
+package com.github.zipcodewilmington;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static String validYN = "yn";
+    private static String validLetter = "abcdefghijklmnopqrstuvwxyz-";
+    private static ArrayList<String> validYNList;
+    private static ArrayList<String> validLetterList;
+    private static String yesNoMessage = "Wanna play again? (\'y\' or \'n\') ";
+    private static String guessMessage = "Enter a single character:  ";
+
+    public static void main(String[] args){
+        validYNList = new ArrayList<>(Arrays.asList(validYN.split("")));
+        validLetterList = new ArrayList<>(Arrays.asList(validLetter.split("")));
+
+        do { } while(run());
+
+        quit();
+    }
+
+    private static boolean run(){
+        System.out.println("Welcome to Hangman ver. 1.0");
+        Hangman mrMan = new Hangman();
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.println("Current guesses: ");
+            if(mrMan.printGuess()){
+                break;
+            }
+
+            // making sure the current guess is valid
+            Character curGuess = getValidInput(in, validLetterList, guessMessage);
+            if(curGuess.equals('-')){
+                quit();
+            }
+            mrMan.makeGuess(curGuess);
+
+        } while(mrMan.isNotMaxGuess());
+        if(!mrMan.isNotMaxGuess()){
+            System.out.println("You ran outta lives :(\n");
+        }
+
+        // see if they wanna play again
+        char yesOrNo = getValidInput(in, validYNList, yesNoMessage);
+        // make pretty line spacing
+        System.out.println("\n");
+
+        return (yesOrNo == 'y');
+    }
+
+    private static char getValidInput(Scanner scan, ArrayList<String> validChars, String prompt){
+        // create a character to test
+        String currentCharInput;
+        // making sure the current response is valid
+        do {
+            System.out.print(prompt);
+            String response = scan.nextLine();
+            currentCharInput = "" + response.toLowerCase().charAt(0);
+
+            if(!validChars.contains(currentCharInput)){
+                System.out.println("You can do better. Try picking a valid character.");
+            }
+        } while(!validChars.contains(currentCharInput));
+
+        return currentCharInput.charAt(0);
+    }
+
+    /*
+     * Stolen from Ricky
+     */
+    private static void quit() {
+        System.exit(0);
+    }
+}
